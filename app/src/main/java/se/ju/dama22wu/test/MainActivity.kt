bdeb44a9e4922935d4e2b82a5eec9eb80387a3cc
dama22wu <57435832+MyMemeisTrue@users.noreply.github.com>
2025-11-20 17:38:32 +0100

    On Hoist state

--- app/src/main/java/se/ju/dama22wu/test/MainActivity.kt
+++ app/src/main/java/se/ju/dama22wu/test/MainActivity.kt
@@ -1,23 +1,34 @@
 package se.ju.dama22wu.test
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import se.ju.dama22wu.test.ui.theme.TestTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +41,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
@@ -37,23 +49,59 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember {mutableStateOf (false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(24.dp))
-        {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)){
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = {expanded.value = !expanded.value}
+            ) {
+                Text( if(expanded.value) "Show less" else "Show more")
+            }
+        }
 
-            Text(text = "Hello")
-            Text(text = name)
+    }
+}
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("Continue")
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen()
+    }
+}
+
+@Composable
+
 fun MyApp(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("world", "compose", "Peace")
